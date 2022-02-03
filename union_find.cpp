a8bd0ed897f48c2ff64d7bd5ca7b49b4e6f68ba9
0kina <itokon20@gmail.com>
2022-02-04 08:32:35 +0900

    UnionFindの追加

---
+++ union_find.cpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+class UnionFind {
+ private:
+  std::vector<int> par, siz;
+
+ public:
+  UnionFind(int n) : par(n), siz(n, 1) {
+      std::iota(par.begin(), par.end(), 0);
+    }
+
+  int root(int x) {
+    if (par[x] == x) {
+      return x;
+    }
+    return par[x] = root(par[x]);
+  }
+
+  int is_same(int x, int y) {
+    return root(x) == root(y);
+  }
+
+  bool unite(int x, int y) {
+    x = root(x);
+    y = root(y);
+
+    if (x == y) return false;
+
+    if (siz[x] < siz[y]) std::swap(x, y);
+
+    par[y] = x;
+    siz[x] += siz[y];
+    return true;
+  }
+
+  int size(int x) {
+    return siz[root(x)];
+  }
+};
