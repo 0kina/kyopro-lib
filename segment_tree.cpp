ab668d18aa24b71f6d2897f00cfe0066ef47a127
0kina <itokon20@gmail.com>
2022-02-13 02:02:38 +0900

    segment_tree.cppで、クエリに対して、解のインデックスも返すように変更

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -5,46 +5,54 @@
 
 template <typename DataType>
 class SegmentTree {
+  using NodeType = std::pair<DataType, int>;
+
  public:
   SegmentTree<DataType>(int n, const DataType identity_elem,
-                        std::function<DataType(DataType, DataType)> fun)
-    : identity_elem(identity_elem), fun(fun) {
-    int siz = 1;
-    while (siz < n) siz *= 2;
-    this->n = siz;
+                        std::function<DataType(DataType, DataType)> val_fun)
+    : identity_elem(identity_elem) {
+    n_leaves = 1;
+    while (n_leaves < n) n_leaves *= 2;
 
-    siz = siz * 2 - 1;
-    val = std::vector<DataType>(siz, identity_elem);
+    val = std::vector<NodeType>(n_leaves * 2 - 1, {identity_elem, -1});
+    for (int i = 0; i < n; ++i) {
+      val[n_leaves - 1 + i].second = i;
+    }
+
+    fun = [=](NodeType a, NodeType b) {
+      DataType tmp = val_fun(a.first, b.first);
+      if (tmp == a.first) return a;
+      else return b;
+    };
   }
 
   void update(int pos, DataType new_val) {
-    pos += n - 1;
-    val[pos] = new_val;
+    pos += n_leaves - 1;
+    val[pos].first = new_val;
     while (pos > 0) {
       pos = (pos - 1) / 2;
       val[pos] = fun(val[2 * pos + 1], val[2 * pos + 2]);
     }
   }
 
-  DataType query(int query_left, int query_right) {
-    return query_main(query_left, query_right, 0, 0, n);
+  NodeType query(int query_left, int query_right) {
+    return query_main(query_left, query_right, 0, 0, n_leaves);
   }
 
  private:
-  int n;
+  int n_leaves;
   DataType identity_elem;
-  std::function<DataType(DataType, DataType)> fun;
-  std::vector<DataType> val;
+  std::function<NodeType(NodeType, NodeType)> fun;
+  std::vector<NodeType> val;
 
-  DataType query_main(int query_left, int query_right, int node, int seg_left, int seg_right) {
-    if (query_left >= seg_right || query_right <= seg_left) return identity_elem;
+  NodeType query_main(int query_left, int query_right, int node, int seg_left, int seg_right) {
+    if (query_left >= seg_right || query_right <= seg_left) return std::make_pair(identity_elem, -1);
 
     if (query_left <= seg_left && query_right >= seg_right) return val[node];
     else {
-      DataType c1 = query_main(query_left, query_right, node * 2 + 1, seg_left, (seg_left + seg_right) / 2);
-      DataType c2 = query_main(query_left, query_right, node * 2 + 2, (seg_left + seg_right) / 2, seg_right);
+      NodeType c1 = query_main(query_left, query_right, node * 2 + 1, seg_left, (seg_left + seg_right) / 2);
+      NodeType c2 = query_main(query_left, query_right, node * 2 + 2, (seg_left + seg_right) / 2, seg_right);
       return fun(c1, c2);
     }
   }
-
 };
