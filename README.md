ed08a56df87bb7f360744c643fee861b78fedaab
0kina <itokon20@gmail.com>
2022-02-13 01:21:06 +0900

    segment_tree.cppを追加

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ## validation
 * dijkstra: Library Checker(https://judge.yosupo.jp/submission/78163)
 * modint: ABC055_B(https://atcoder.jp/contests/abc055/submissions/29173328)
+* segment_tree: Range Minimum Query (RMQ)(https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=6317952#1)
 * union_find: Library Checker(https://judge.yosupo.jp/submission/76902)
---
+++ segment_tree.cpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+template <typename DataType>
+class SegmentTree {
+ public:
+  SegmentTree<DataType>(int n, const DataType identity_elem,
+                        std::function<DataType(DataType, DataType)> fun)
+    : identity_elem(identity_elem), fun(fun) {
+    int siz = 1;
+    while (siz < n) siz *= 2;
+    this->n = siz;
+
+    siz = siz * 2 - 1;
+    val = std::vector<DataType>(siz, identity_elem);
+  }
+
+  void update(int pos, DataType new_val) {
+    pos += n - 1;
+    val[pos] = new_val;
+    while (pos > 0) {
+      pos = (pos - 1) / 2;
+      val[pos] = fun(val[2 * pos + 1], val[2 * pos + 2]);
+    }
+  }
+
+  DataType query(int query_left, int query_right) {
+    return query_main(query_left, query_right, 0, 0, n);
+  }
+
+ private:
+  int n;
+  DataType identity_elem;
+  std::function<DataType(DataType, DataType)> fun;
+  std::vector<DataType> val;
+
+  DataType query_main(int query_left, int query_right, int node, int seg_left, int seg_right) {
+    if (query_left >= seg_right || query_right <= seg_left) return identity_elem;
+
+    if (query_left <= seg_left && query_right >= seg_right) return val[node];
+    else {
+      DataType c1 = query_main(query_left, query_right, node * 2 + 1, seg_left, (seg_left + seg_right) / 2);
+      DataType c2 = query_main(query_left, query_right, node * 2 + 2, (seg_left + seg_right) / 2, seg_right);
+      return fun(c1, c2);
+    }
+  }
+
+};
