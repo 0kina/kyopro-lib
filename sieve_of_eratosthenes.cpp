6cd5c54f8f0804a09da33fb7a55afa4da2a311c9
0kina <itokon20@gmail.com>
2022-06-26 09:47:38 +0900

    素数列挙関数を追加

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -16,3 +16,22 @@ std::vector<bool> sieve_of_eratosthenes(int n) {
 
   return res;
 }
+
+std::vector<int> enum_prime_nums(int n) {
+  std::vector<bool> is_prime(n + 1, true);
+  is_prime[0] = false;
+  is_prime[1] = false;
+
+  std::vector<int> res;
+  for (int i = 2; i < n; ++i) {
+    if (!is_prime[i]) continue;
+
+    res.push_back(i);
+
+    for (int j = 2 * i; j <= n; j += i) {
+      is_prime[j] = false;
+    }
+  }
+
+  return res;
+}
