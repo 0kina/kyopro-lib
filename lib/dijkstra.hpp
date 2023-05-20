65a9025d6a097bd73c0e38ff98259da6636723ac
0kina <itokon20@gmail.com>
2023-05-21 00:50:28 +0900

    Update dijkstra.hpp

--- lib/dijkstra.hpp
+++ lib/dijkstra.hpp
@@ -1,5 +1,4 @@
-#ifndef OKINA_DIJKSTRA
-#define OKINA_DIJKSTRA
+#pragma once
 
 #include <algorithm>
 #include <queue>
@@ -9,18 +8,61 @@
 template <typename CostType>
 class Dijkstra {
   using State = std::pair<CostType, int>;
- public:
-  Dijkstra(const int n, const int src): _src(src), _g(n), _dist(n, _INF), _prev(n, -1) {
+public:
+  Dijkstra(const std::vector<std::vector<pair<int, CostType>>> &g, int src)
+    : _src(src), _g(g), _dist(g.size(), _INF), _prev(g.size(), -1)
+  {
     _dist[src] = 0;
-
     _pq.emplace(0, src);
+    _solve();
   }
 
-  void add_edge(const int u, const int v, const CostType c) {
-    _g[u].emplace_back(v, c);
+  Dijkstra(const std::vector<std::vector<int>> &g, int src)
+    : _src(src), _g(g.size()), _dist(g.size(), _INF), _prev(g.size(), -1)
+  {
+    for (int i = 0; i < g.size(); ++i) {
+      for (int j : g[i]) {
+        _g[i].push_back(std::make_pair(j, 1));
+      }
+    }
+    _dist[src] = 0;
+    _pq.emplace(0, src);
+    _solve();
   }
 
-  void solve() {
+  bool is_reachable(int t) const {
+    return _dist[t] != _INF;
+  }
+
+  CostType distance_to(int t) const {
+    return _dist[t];
+  }
+
+  std::vector<int> path_to(int t) const {
+    if (!is_reachable(t)) return std::vector<int>();
+    std::vector<int> res;
+
+    res.push_back(t);
+    int pos = t;
+    while (pos != _src) {
+      res.push_back(_prev[pos]);
+      pos = _prev[pos];
+    }
+
+    std::reverse(std::begin(res), std::end(res));
+
+    return res;
+  }
+
+private:
+  const CostType _INF = std::numeric_limits<CostType>::max();
+  int _src;
+  std::vector<std::vector<std::pair<int, CostType>>> _g;
+  std::vector<CostType> _dist;
+  std::vector<int> _prev;
+  std::priority_queue<State, std::vector<State>, std::greater<State>> _pq;
+
+  void _solve() {
     while (!_pq.empty()) {
       auto [du, u] = _pq.top();
 
@@ -36,43 +78,5 @@ class Dijkstra {
         }
       }
     }
-    _is_solved = true;
   }
-
-  bool is_reachable(int t) const {
-    assert(_is_solved);
-    return _dist[t] != _INF;
-  }
-
-  CostType distance_to(int t) const {
-    assert(is_reachable(t));
-    return _dist[t];
-  }
-
-  std::vector<int> path_to(int t) const {
-    assert(is_reachable(t));
-    std::vector<int> res;
-
-    res.push_back(t);
-    int pos = t;
-    while (pos != _src) {
-      res.push_back(_prev[pos]);
-      pos = _prev[pos];
-    }
-
-    std::reverse(std::begin(res), std::end(res));
-
-    return res;
-  }
-
- private:
-  const CostType _INF = std::numeric_limits<CostType>::max();
-  int _src;
-  std::vector<std::vector<std::pair<int, CostType>>> _g;
-  std::vector<CostType> _dist;
-  std::vector<int> _prev;
-  std::priority_queue<State, std::vector<State>, std::greater<State>> _pq;
-  bool _is_solved = false;
 };
-
-#endif
